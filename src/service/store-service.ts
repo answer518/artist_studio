2491e3349be9e8ca58ec459a9ac023be10dfc31e
guotingjie <guotingjie@lzzg365.com>
2025-08-26 20:24:41 +0800

    feat: 文章草稿功能实现

--- src/service/store-service.ts
+++ src/service/store-service.ts
@@ -1,6 +1,32 @@
-export const getArticle = async (title: string) => {};
-export const saveArticle = async (title: string, content: string) => {};
-export const deleteArticle = async (title: string) => {};
+export const getArticle = async (title: string) => {
+  let aid = `article-${title}`;
+  return localStorage.getItem(aid);
+};
+export const saveArticle = async (title: string, content: string) => {
+  let aid = `article-${title}`;
+  localStorage.setItem(aid, content);
+};
+export const deleteArticle = async (title: string) => {
+  let aid = `article-${title}`;
+  localStorage.removeItem(aid);
+  return getArticleList();
+};
 export const getArticleList = async () => {
-  return [];
+  let length = localStorage.length;
+  let rst = [];
+  let index = 0;
+  for (let i = 0; i < length; i++) {
+    let aid = localStorage.key(i);
+    if (aid?.startsWith("article-")) {
+      let title = aid.split("-")[1];
+      index++;
+      rst.push({
+        index,
+        title,
+        content: localStorage.getItem(aid),
+      });
+    }
+  }
+
+  return rst;
 };
